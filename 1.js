683e150422ac0f5c2efc6c04ee4d948228bed706
Alfian Horis <alfian.horis@merahciptamedia.co.id>
2023-02-25 10:37:04 +0700

    commit

---
+++ 1.js
@@ -0,0 +1,5 @@
+//1. buatlah 1 variable dengan nama myFriends
+//2. isi variable myFriends dengan value integer 100
+//3. buatlah sebuah variable dengan nama villain dengan value integer 50
+//4. buatlah sebuah variable total dengan berisi value pengurangan variable myfriends dengan variable villain
+//5. tampilkan variable total dengan menggunakan console.log
